e0d845895cb17dd0da62b158ebc2eb0384fc5f60
Bruno Martins <bgcngm@gmail.com>
2020-09-22 14:22:40 +0100

    libjson: Replace copy headers with a header library
    Change-Id: Iaf85569c8328b927ce366b834bef9346f8c8b55a

--- Android.mk
+++ Android.mk
@@ -3,20 +3,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libjson
 LOCAL_PATH := $(LIBJSON_ROOT)
 LOCAL_CFLAGS := -Werror -Wno-unused-parameter
-LOCAL_COPY_HEADERS_TO := libjson/inc
-LOCAL_COPY_HEADERS := bits.h \
-		config.h \
-		debug.h \
-		linkhash.h \
-		arraylist.h \
-		json.h \
-		json_config.h \
-		json_inttypes.h \
-		json_util.h \
-		json_object.h \
-		json_tokener.h \
-		json_object_iterator.h \
-		json_c_version.h
 LOCAL_SRC_FILES := arraylist.c \
 		debug.c \
 		json_c_version.c \
@@ -29,6 +15,12 @@ LOCAL_SRC_FILES := arraylist.c \
 		printbuf.c \
 		random_seed.c
 LOCAL_SHARED_LIBRARIES := libcutils libutils
+LOCAL_HEADER_LIBRARIES := libjson_headers
 LOCAL_MODULE_TAG := optional
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libjson_headers
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
+include $(BUILD_HEADER_LIBRARY)
